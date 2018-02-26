25f9c0bb2fc25944797e11c247db094edcd4cf9b
Paolo Carrara <paolocarraratux@gmail.com>
2018-02-26 20:42:22 -0300

    Creates the first and second activities and the functionality to go from activitie one to activitie two.

--- app/src/main/java/com/nastydevs/changeactivities/MainActivity.kt
+++ app/src/main/java/com/nastydevs/changeactivities/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.nastydevs.changeactivities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToNextActivity (view: View) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
