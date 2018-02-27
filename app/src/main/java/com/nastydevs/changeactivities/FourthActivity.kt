07d2c95778e5aa759c7fe45744f4acb1f00060c4
Paolo Carrara <paolocarraratux@gmail.com>
2018-02-27 20:28:56 -0300

    Adds an activity without an action bar.

--- app/src/main/java/com/nastydevs/changeactivities/FourthActivity.kt
+++ app/src/main/java/com/nastydevs/changeactivities/FourthActivity.kt
@@ -1,7 +1,9 @@
 package com.nastydevs.changeactivities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class FourthActivity : AppCompatActivity() {
 
@@ -18,5 +20,10 @@ class FourthActivity : AppCompatActivity() {
         // The new activity is kept still and this activity is pushed out to the left
         overridePendingTransition(R.anim.hold, R.anim.push_out_to_left)
         super.onPause()
+    }
+
+    fun goToNextActivity (view: View) {
+        val intent = Intent(this, FifthActivity::class.java)
+        startActivity(intent)
     }
 }
