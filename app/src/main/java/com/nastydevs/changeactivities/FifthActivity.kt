c0cff6e9643d6458d1dece28f082e7818badf26f
Paolo Carrara <paolocarraratux@gmail.com>
2018-02-28 00:21:58 -0300

    Adds an activity full of buttons to change the background color of the current activity.

--- app/src/main/java/com/nastydevs/changeactivities/FifthActivity.kt
+++ app/src/main/java/com/nastydevs/changeactivities/FifthActivity.kt
@@ -1,12 +1,20 @@
 package com.nastydevs.changeactivities
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class FifthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fifth)
+    }
+
+    fun changeColor (view: View) {
+        val color = Color.parseColor(view.tag.toString())
+        window.decorView.setBackgroundColor(color)
+        view.setBackgroundColor(color)
     }
 }
